7770a086d4b851dca6244056ae3a1feffea2a801
pione30 <g.pionelish30@gmail.com>
2019-09-30 00:23:22 +0900

    add rectangle

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
